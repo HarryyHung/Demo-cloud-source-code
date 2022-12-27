46d78dfa1c5a147f9296b6a61dab058be70c8363
HungBuii <buiduyhungg2310@gmail.com>
2022-12-27 17:02:03 +0700

    Update 3

--- Webb2/databaseHandler.js
+++ Webb2/databaseHandler.js
@@ -4,36 +4,36 @@ const { Int32, ObjectId } = require('bson')
 
 async function insertProduct(newProduct) {
     let client = await MongoClient.connect(url)
-    let db = client.db("GCH1003")
+    let db = client.db("GCH1002")
     let newId = await db.collection("products").insertOne(newProduct)
     return newId
 }
 async function getAllProducts() {
     let client = await MongoClient.connect(url)
-    let db = client.db("GCH1003")
+    let db = client.db("GCH1002")
     const results = await db.collection("products").find().toArray()
     return results
 }
 async function deleteProductById(id) {
     let client = await MongoClient.connect(url)
-    let db = client.db("GCH1003")
+    let db = client.db("GCH1002")
     await db.collection("products").deleteOne({ _id: ObjectId(id) })
 }
 async function updateProduct(id, name, price, picture) {
     let client = await MongoClient.connect(url)
-    let db = client.db("GCH1003")
+    let db = client.db("GCH1002")
     await db.collection("products").updateOne({ _id: ObjectId(id) },
         { $set: { "name": name, "price": price, "pictureURL": picture } })
 }
 async function findProductById(id) {
     let client = await MongoClient.connect(url)
-    let db = client.db("GCH1003")
+    let db = client.db("GCH1002")
     const productToEdit = await db.collection("products").findOne({ _id: ObjectId(id) })
     return productToEdit
 }
 async function searchProductByName(name){
     let client = await MongoClient.connect(url)
-    let db = client.db("GCH1003")
+    let db = client.db("GCH1002")
     const results = await db.collection("products").find({ name: new RegExp(name,'i') }).toArray()
     return results
 }
--- Webb2/main.js
+++ Webb2/main.js
@@ -1,65 +1,82 @@
 const { Int32, ObjectId } = require('bson')
 var express = require('express')
-const { insertProduct,getAllProducts,
-        deleteProductById,updateProduct,findProductById,searchProductByName } = require('./databaseHandler')
+const { insertProduct, getAllProducts,
+    deleteProductById, updateProduct, findProductById, searchProductByName } = require('./databaseHandler')
 var app = express()
 
-app.set('view engine','hbs')
-app.use(express.urlencoded({extended:true}))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 
 
-app.post('/search',async (req,res)=>{
+app.post('/search', async (req, res) => {
     const search = req.body.search
     const results = await searchProductByName(search)
     console.log(results)
-    res.render('view',{'results':results})
+    res.render('view', { 'results': results })
 })
 
-app.post('/edit',async (req,res)=>{
+app.post('/edit', async (req, res) => {
     const id = req.body.id
     const name = req.body.txtName
     const price = req.body.txtPrice
     const picture = req.body.txtPic
-    await updateProduct(id, name, price, picture)
+    const type = req.body.txtType
+    await updateProduct(id, name, price, picture, type)
     res.redirect('/view')
 })
 
-app.get('/edit',async (req,res)=>{
+app.get('/edit', async (req, res) => {
     const id = req.query.id
     const productToEdit = await findProductById(id)
-    res.render('edit',{product:productToEdit})
+    res.render('edit', { product: productToEdit })
 })
 
-app.get('/delete',async (req,res)=>{
+app.get('/delete', async (req, res) => {
     const id = req.query.id
     await deleteProductById(id)
     res.redirect('/view')
 })
 
-app.get('/view',async (req,res)=>{
+app.get('/view', async (req, res) => {
     const results = await getAllProducts()
-    res.render('view',{'results':results})
+    res.render('view', { 'results': results })
 })
 
-app.post('/new',async (req,res)=>{
+app.post('/new', async (req, res) => {
     let name = req.body.txtName
     let price = req.body.txtPrice
     let picture = req.body.txtPic
+    let type = req.body.txtType
     let newProduct = {
-        name : name,
-        price: Number.parseInt(price) ,
-        pictureURL: picture
+        name: name,
+        price: Number.parseInt(price),
+        pictureURL: picture,
+        type: type
     }
-    let newId = await insertProduct(newProduct)
-    console.log(newId.insertedId)
-    res.render('home')
+    if (name.length > 22) {
+        let resultError = {
+            nameError: 'You must enter name has length < 35!',
+        };
+        res.render('newProduct', {  results: resultError });
+    }
+    else if (isNaN(price)) {
+        let resultError1 = {
+            price: "Enter number"
+        };
+        res.render('newProduct', { results: resultError1 });
+    }
+    else {
+        let newId = await insertProduct(newProduct)
+        console.log(newId.insertedId)
+        res.render('home')
+    }
 })
 
-app.get('/new',(req,res)=>{
+app.get('/new', (req, res) => {
     res.render('newProduct')
 })
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('home')
 })
 
